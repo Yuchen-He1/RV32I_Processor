84320feffee6f3c98c02bf187cf0d90d01481176
Yuchen <yuchenhe@ucsb.edu>
2025-03-22 13:33:16 -0700

    create all module needed for single cycle processor

---
+++ immgen.v
@@ -0,0 +1,9 @@
+// this file is immediate number generator for single cycle processor.
+// ISA based on RV32I
+
+module immgen (
+    input [31:0] instruction,
+    output reg [31:0] imm
+);
+    
+endmodule
